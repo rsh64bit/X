ee3c80fd252d09c8b809aec0918c0ba080b1fa84
ravi sharma <rsharma.lnx@gmail.com>
2026-06-03 16:03:41 +0100

    cpp: lcc-3

--- x-lc/lcc-3/cxx/lcc-3.cpp
+++ x-lc/lcc-3/cxx/lcc-3.cpp
@@ -1,12 +1,12 @@
 /** Longest Substring Without Repeating Characters
-Medium
+  Medium
 
 
-Given a string s, find the length of the longest substring without duplicate characters.
+  Given a string s, find the length of the longest substring without duplicate characters.
 
- 
 
-Example 1:
+
+  Example 1:
 
 Input: s = "abcabcbb"
 Output: 3
@@ -24,101 +24,148 @@ Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-#include<iostream>
-#include<vector>
+/* --std=c++20*/
 
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <climits>
+#include <cstdint>
 
 template<typename T>
 std::vector<T> add_result(const T& s, const T& i){
-  return {s, i};
+    return {s, i};
 }
 template<typename T>
 class Var_Sli_Window {
 
-private:
-std::vector<T> data;
-std::vector<std::vector<T>> result;
+    private:
+        std::vector<T> data;
+        std::vector<std::vector<T>> result;
 
-public:
-  Var_Sli_Window (std::vector<T> d) : data(std::move(d)) {}
- void prn_win() {
-   for (const auto& x : data)
-     std::cout << x << " ";
-     std::cout <<  "\n";
-     for (const auto& z : result) {
-     std::cout <<  " sum and items count ";
-     for (const auto& y : z)
-      std::cout << y << " ";
-     std::cout <<  "\n";
-     }
- }
-
- int32_t min_win(uint32_t val) {
-    int32_t l = 0 , r = 0, s = 0;
-    int32_t min = INT_MAX;
-    for (int32_t r = 0; r < data.size(); ++r) {
-        s += data[r];
-        while (s >= val) {
-          min = std::min(min, r - l + 1);
-          s -= data[l];
-          l++;
+    public:
+        Var_Sli_Window (std::vector<T> d) : data(std::move(d)) {}
+        void prn_win() {
+            for (const auto& x : data)
+                std::cout << x << " ";
+            std::cout <<  "\n";
+            for (const auto& z : result) {
+                std::cout <<  " sum and items count ";
+                for (const auto& y : z)
+                    std::cout << y << " ";
+                std::cout <<  "\n";
+            }
         }
-    }
-    return min == INT_MAX ? 0 : min;
-  }
 
-  void sub_win2(uint32_t val) {
-    int32_t l = 0 , r = 0, s = 0;
-
-    for(r = 0; r < data.size(); ++r) {
-        s += data[r];;
-      while (s >= val) {
-        s -= data[l]; ++l;
-        if (s == val) {
-          std::vector<int> t = add_result(s, r - l + 1);
-          result.push_back(t);
+        int32_t min_win(uint32_t val) {
+            int32_t l = 0 , r = 0, s = 0;
+            int32_t min = INT_MAX;
+            for (int32_t r = 0; r < data.size(); ++r) {
+                s += data[r];
+                while (s >= val) {
+                    min = std::min(min, r - l + 1);
+                    s -= data[l];
+                    l++;
+                }
+            }
+            return min == INT_MAX ? 0 : min;
         }
-      }
-    }
-  }
 
-  /*inefficient way to print all the size of subarray sum*/
-  void sub_win(uint32_t val) {
-    int32_t l = 0 , r = 0, s = 0;
+        void sub_win2(uint32_t val) {
+            int32_t l = 0 , r = 0, s = 0;
 
-    while (r < data.size()) {
-      while (r < data.size() && s <= val) {
-        s += data[r]; r++;
-        if (s == val) {
-          auto t = add_result(s, r - l);
-          result.push_back(t);
+            for(r = 0; r < data.size(); ++r) {
+                s += data[r];;
+                while (s >= val) {
+                    s -= data[l]; ++l;
+                    if (s == val) {
+                        std::vector<int> t = add_result(s, r - l + 1);
+                        result.push_back(t);
+                    }
+                }
+            }
         }
-      }
-      while (s >= val && l < data.size()) {
-        s -= data[l]; ++l;
-        if (s == val) {
-          std::vector<int> t = add_result(s, r - l);
-          result.push_back(t);
+
+        /*inefficient way to print all the size of subarray sum*/
+        void sub_win(uint32_t val) {
+            int32_t l = 0 , r = 0, s = 0;
+
+            while (r < data.size()) {
+                while (r < data.size() && s <= val) {
+                    s += data[r]; r++;
+                    if (s == val) {
+                        auto t = add_result(s, r - l);
+                        result.push_back(t);
+                    }
+                }
+                while (s >= val && l < data.size()) {
+                    s -= data[l]; ++l;
+                    if (s == val) {
+                        std::vector<int> t = add_result(s, r - l);
+                        result.push_back(t);
+                    }
+                }
+            }
         }
-      }
-    }
-  }
 };
+
+class Longest {
+    private:
+        std::vector<std::string> str;
+    public:
+
+        void prn_str() {
+            for (const auto& x : str) {
+                std::cout << x;
+                std::cout << std::endl;
+            }
+        }
+
+        void sub_string(std::string s) {
+            int l = 0, j = 0;
+            std::unordered_set<char> hash;
+            int n = 0;
+            std::string temp, longest;
+            /*expand, shrink*/
+            for (j = 0; j < s.size(); ++j) {
+                if (!hash.contains(s[j])) {
+                    longest.push_back(s[j]);
+                    hash.insert(s[j]);
+                }
+                else {
+                    /*shrink*/
+                    for (; l < j; ++l) {
+                        temp.push_back(s[l]);
+                        hash.erase(s[l]);
+                    }
+                    if(temp.size()) {
+                        str.push_back(temp);
+                        temp.clear();
+                        longest.clear();
+                    }
+                }
+            }
+            //when there is no shrink to do
+            if ( l != j - 1)
+                        str.push_back(longest);
+        }
+};
+
 int main() {
 
 
-  std::vector<int> v{2, 3, 1, 2, 4, 3};
-  std::vector<int> e{2, 3};
+    std::vector<int> v{2, 3, 1, 2, 4, 3};
+    std::vector<int> e{2, 3};
+    Var_Sli_Window<int> w(v), l(e);
+    w.sub_win2(7);
+    w.prn_win();
+    l.sub_win2(7);
+    l.prn_win();
+    std::cout << w.min_win(7) <<std::endl;
+    Longest long_str;
+    long_str.sub_string("abcalkmnopnpat");
+    long_str.prn_str();
 
-  Var_Sli_Window<int> w(v), l(e);
-  w.sub_win2(7);
-  w.prn_win();
-
-  l.sub_win2(7);
-  l.prn_win();
-
-  std::cout << w.min_win(7) <<std::endl;
-
-  return 0;
+    return 0;
 }
 
