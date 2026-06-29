9ac61cf4534235e859f0139893b5cc26f35f6d07
ravi sharma <rsharma.lnx@gmail.com>
2026-06-29 16:26:43 +0100

    lcc: lcc-4 cosmetic changes
    Signed-off-by: ravi sharma <rsharma.lnx@gmail.com>

--- x-lc/lcc-4/cpp/lcc-4.cpp
+++ x-lc/lcc-4/cpp/lcc-4.cpp
@@ -1,23 +1,15 @@
 /*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-
   The overall run time complexity should be O(log (m+n)).
 
-
-
-  Example 1:
-
+Example 1:
 Input: nums1 = [1,3], nums2 = [2]
 Output: 2.00000
 Explanation: merged array = [1,2,3] and median is 2.
 Example 2:
-
 Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
-
-
 Constraints:
-
 nums1.length == m
 nums2.length == n
 0 <= m <= 1000
@@ -32,12 +24,9 @@ nums2.length == n
 class Solution {
 
     public:
-
         /*part of merge sort used here*/
         void findmediansortedarrays2(std::vector<int>& n1, std::vector<int>& n2) {
                 //std::vector<int> n1 = {1, 3 ,5, 6}, n2 = {4, 9 };
-                
-
             std::vector<int> sort;
             int l = 0, m = 0;
 
